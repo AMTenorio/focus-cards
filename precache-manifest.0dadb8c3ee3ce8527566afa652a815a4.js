e4a97a8f30a8a756f3a63ff7d5d210d9fd322951
Angelo Tenorio <tenorioam@hotmail.com>
2019-03-20 12:06:09 -0700

    Updates

---
+++ precache-manifest.0dadb8c3ee3ce8527566afa652a815a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9391f1fef92ec849259c93044c28be00",
+    "url": "/focus-cards/./static/media/Alarm.9391f1fe.mp3"
+  },
+  {
+    "revision": "ea22eac723737fc41197",
+    "url": "/focus-cards/./static/js/runtime~main.bbe237d2.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/focus-cards/./static/js/main.5ef2265d.chunk.js"
+  },
+  {
+    "revision": "f1e58027e70ca4be5b2b",
+    "url": "/focus-cards/./static/js/2.35bc16c8.chunk.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/focus-cards/./static/css/main.aebecd1f.chunk.css"
+  },
+  {
+    "revision": "a15cf5014117738040c1c87041b6cf44",
+    "url": "/focus-cards/./index.html"
+  }
+];
