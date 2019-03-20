74b5b10325f2e629b96af773903faada3f8b146b
Angelo Tenorio <tenorioam@hotmail.com>
2019-03-20 12:18:15 -0700

    Updates

---
+++ precache-manifest.6124f1a66658db8becd29e5c97c307a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9391f1fef92ec849259c93044c28be00",
+    "url": "/focus-cards/static/media/Alarm.9391f1fe.mp3"
+  },
+  {
+    "revision": "4b2e3397c7ecf8bd3aa4",
+    "url": "/focus-cards/static/js/runtime~main.c90ef1aa.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/focus-cards/static/js/main.5ef2265d.chunk.js"
+  },
+  {
+    "revision": "f1e58027e70ca4be5b2b",
+    "url": "/focus-cards/static/js/2.35bc16c8.chunk.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/focus-cards/static/css/main.aebecd1f.chunk.css"
+  },
+  {
+    "revision": "c62fe8487c333a4daf1f40f434d881c9",
+    "url": "/focus-cards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.7f90fa520b1b89944b93f1f2e89401b5.js"
+  "/focus-cards/precache-manifest.6124f1a66658db8becd29e5c97c307a1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/focus-cards/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
