ddf22fd84354918c2cbae85f2a4d9ba2f2f7c578
Angelo Tenorio <tenorioam@hotmail.com>
2019-03-20 12:16:11 -0700

    Updates

---
+++ precache-manifest.7f90fa520b1b89944b93f1f2e89401b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9391f1fef92ec849259c93044c28be00",
+    "url": "/myapp/static/media/Alarm.9391f1fe.mp3"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/myapp/static/js/main.5ef2265d.chunk.js"
+  },
+  {
+    "revision": "f1e58027e70ca4be5b2b",
+    "url": "/myapp/static/js/2.35bc16c8.chunk.js"
+  },
+  {
+    "revision": "72d9ecc19f4401237fc7",
+    "url": "/myapp/static/css/main.aebecd1f.chunk.css"
+  },
+  {
+    "revision": "f555e0c89d8932919e6e02efda27ead1",
+    "url": "/myapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4004b53d4e62202872c3702fa9925f40.js"
+  "/myapp/precache-manifest.7f90fa520b1b89944b93f1f2e89401b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/myapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
